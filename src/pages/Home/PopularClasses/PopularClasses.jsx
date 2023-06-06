89ed76d2a1a5f7e124c2a121880e8e6101213e81
Md.Mosarof Hossen <mosarofhossen5228@gmail.com>
2023-06-07 02:17:58 +0600

    react helmet and popular classes component has declear

---
+++ src/pages/Home/PopularClasses/PopularClasses.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+
+
+const PopularClasses = () => {
+    return (
+        <div>
+            <Helmet>
+                <title>A.Y Meditation || Popular Classes</title>
+            </Helmet>
+
+            <h2>this is popular classes</h2>
+        </div>
+    );
+};
+
+export default PopularClasses;
