a2cbf97be94bd0e95ce215d0e5b0fe4ddb4705b4
Md.Mosarof Hossen <mosarofhossen5228@gmail.com>
2023-06-16 23:19:18 +0600

    popular instructor has done

--- src/pages/Users/Instructor.jsx
+++ src/pages/Users/Instructor.jsx
@@ -13,17 +13,19 @@ const Instructor = () => {
 
     return (
         <div>
-            <h2>this is instructor</h2>
-            <p>{popularInstructors.length}</p>
+            <h2 className='uppercase text-center font-bold text-3xl'>Popular Instructor</h2>
+
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-4'>
                 {
                     popularInstructors.map(popularInstructor =>
                         <div>
-                            <div className="card w-96 bg-base-100 shadow-xl">
-                                <figure><img src={popularInstructor.image} alt="Shoes" /></figure>
+                            <div className="card w-96 bg-base-100 shadow-xl ">
+
                                 <div className="card-body">
-                                    <h2 className="card-title">Shoes!</h2>
+                                    <h2 className="card-title">{popularInstructor.name}</h2>
+                                    <p>{popularInstructor.email}</p>
                                     <p>If a dog chews shoes whose shoes does he choose?</p>
+
                                     <div className="card-actions justify-end">
                                         <button className="btn btn-primary">Buy Now</button>
                                     </div>
