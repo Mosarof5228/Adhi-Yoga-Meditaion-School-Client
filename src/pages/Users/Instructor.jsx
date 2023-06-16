e94351fd5e427a88a2f00ee15ba52bbae8796875
Md.Mosarof Hossen <mosarofhossen5228@gmail.com>
2023-06-16 23:22:44 +0600

    instractor page has done

--- src/pages/Users/Instructor.jsx
+++ src/pages/Users/Instructor.jsx
@@ -24,7 +24,7 @@ const Instructor = () => {
                                 <div className="card-body">
                                     <h2 className="card-title">{popularInstructor.name}</h2>
                                     <p>{popularInstructor.email}</p>
-                                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                                    <p>Instructor Person</p>
 
                                     <div className="card-actions justify-end">
                                         <button className="btn btn-primary">Buy Now</button>
